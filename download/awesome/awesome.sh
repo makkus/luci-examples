b72ce5547b664b8cd0354c9cca56b715002b86ed
Markus Binsteiner <makkus@posteo.de>
2018-03-16 16:36:05 +0700

    updated urls

--- download/awesome/awesome.sh
+++ download/awesome/awesome.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #
+# url:
+#   default: https://raw.githubusercontent.com/makkus/luci-examples/master/download/awesome/awesome.sh
+#   version: https://raw.githubusercontent.com/makkus/luci-examples/{{ version }}/download/awesome/awesome.sh
+#
 # Copyright Markus Binsteiner, 2018
-#                       version 0.1
+#                       version 0.2
 
 echo "Awesome!"
