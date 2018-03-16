b338010055e59f6154a06867c2b37ad6930aff5c
Markus Binsteiner <makkus@posteo.de>
2018-03-16 17:36:50 +0700

    Added lucify start/stop directives

--- download/awesome/awesome.sh
+++ download/awesome/awesome.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 #
+# lucify: start
+#
 # url:
 #   default: https://raw.githubusercontent.com/makkus/luci-examples/master/download/awesome/awesome.sh
 #   version: https://raw.githubusercontent.com/makkus/luci-examples/{{ version }}/download/awesome/awesome.sh
+#
+# lucify: stop
 #
 # Copyright Markus Binsteiner, 2018
 #                       version 0.2
